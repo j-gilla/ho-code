9c19602e2be07309fd108ade89c6cc6e26ec4d2c
gilbertjacke <gilbertjacke@gmail.com>
2017-08-30 00:01:40 +0100

    4 tests passing - both routes

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,26 @@ router.get('/clinics/postcode/:postcode', (req, res) => {
     res.status(200).json(filterResults);
 });
 
+router.get('/clinics/city/:name',(req, res) => {
+  let params = req.params.name;
+  let results = data.result;
+  let resObj = {results: {}};
+
+  let filterResults = results.filter((item) => {
+    if(params === item.city){
+      return true;
+    }
+  }).map((item) => item.partial_postcode).reduce((prev,curr) => {
+    let nextItem = prev;
+    if(Object.keys(nextItem).includes(curr)){
+      nextItem[curr] += 1;
+    } else{
+      nextItem[curr] = 1;
+    }
+    return nextItem;
+  },{});
+  resObj.results = filterResults;
+  res.status(200).json(resObj);
+});
+
 module.exports = router;
--- spec/spec.index.js
+++ spec/spec.index.js
@@ -5,7 +5,7 @@ const expect = require('chai').expect;
 const app = require('../app');
 const request = require('supertest');
 
-let correct = [
+let correctPostcodeData = [
   {
     "organisation_id": "58961",
     "name": "Edridge Road Community Health Centre"
@@ -16,7 +16,7 @@ let correct = [
   }
 ];
 
-let incorrect = [
+let incorrectPostcodeData = [
   {
     "organisation_id": "588592",
     "name": "Muggeridge Road Community Wealth Centre"
@@ -28,24 +28,62 @@ let incorrect = [
 ];
 
 describe('/clinics/postcode/CR91PJ', function (done) {
-  it('returns a status code of 200 and provide a JSON response with results that match the full postcode only', (done) =>{
+  it('returns a status code of 200 and provides a JSON response with results that match the full postcode only', (done) =>{
     request(app)
     .get('/clinics/postcode/CR91PJ')
     .expect(200)
     .expect((response) => {
-      expect(res.body.status).to.eql(correct);
+      expect(res.body.status).to.eql(correctPostcode);
     });
     done();
   })
 });
 
-describe('/clinics/postcode/CR9 1PJ', function (done) {
-  it('throws an error when incorrect data is servered', (done) =>{
+describe('/clinics/postcode/CR91PJ', function (done) {
+  it('throws and error when incorrect data is servered', (done) =>{
     request(app)
     .get('/clinics/postcode/CR91PJ')
     .expect(404)
     .expect((response) => {
-      expect(res.body.status).to.eql(incorrect);
+      expect(res.body.status).to.eql(incorrectPostcodeData);
+    });
+    done();
+  })
+});
+
+let correctCityData = {
+    "results": {
+        "CR9": 2,
+        "CR0": 8
+    }
+}
+
+let incorrectCityData = {
+  "results": {
+      "CR9": 22222222,
+      "CR0": 897868
+  }
+}
+
+describe('/clinics/city/:name', function (done) {
+  it('returns a status code of 200 and provides a JSON response with a results object that contains all of the partial_postcodes found and how many of them where found', (done) =>{
+    request(app)
+    .get('/clinics/city/:name')
+    .expect(200)
+    .expect((response) => {
+      expect(res.body.status).to.eql(correctCityData);
+    });
+    done();
+  })
+});
+
+describe('/clinics/city/:name', function (done) {
+  it('throws and error when incorrect data is servered', (done) =>{
+    request(app)
+    .get('/clinics/city/:name')
+    .expect(404)
+    .expect((response) => {
+      expect(res.body.status).to.eql(incorrectCityData);
     });
     done();
   })
